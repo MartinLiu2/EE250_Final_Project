ef77c3873dcf11ef635b987e38f08b7a50297eba
Martin Liu <mlliu@usc.edu>
2021-11-16 17:39:28 -0800

    Made flask app

---
+++ website.py
@@ -0,0 +1,15 @@
+from flask import Flask, request, render_template
+app = Flask(__name__)
+
+intruder_status = True
+
+@app.route('/')
+def index():
+    return render_template('website_disarmed.html')
+
+@app.route('/alarm_armed')
+def alarm_armed():
+    return render_template('website_armed.html', Intruder=intruder_status)
+
+if __name__ == "__main__":
+    app.run(debug=True)
