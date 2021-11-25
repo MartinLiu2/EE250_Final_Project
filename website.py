da1ab7f0f54fa98f7094b77075079d53e8dbefd8
Bailey <bcislows@usc.edu>
2021-11-25 00:22:50 -0800

    kinda dont remember but we did some stuff

--- website.py
+++ website.py
@@ -1,7 +1,30 @@
 from flask import Flask, request, render_template
+from flask_mqtt import Mqtt
+
 
 app = Flask(__name__)
 intruder_status = True
+
+app.config['MQTT_BROKER_URL'] = 'eclipse.usc.edu'
+app.config['MQTT_BROKER_PORT'] = 1883
+app.config['MQTT_KEEPALIVE'] = 60
+
+mqtt = Mqtt(app)
+
+@mqtt.on_topic('bailey/ultrasonicRanger')
+def ultrasonic_callback(client, userdata, msg):
+    intruder_status = msg.payload.decode("utf-8", "strict")
+    # Take output and give it to website.py to determine if door close/open
+
+@mqtt.on_connect()
+def handle_connect(client, userdata, flags, rc):
+    client.subscribe('bailey/ultrasonicRanger')
+    client.message_callback_add('bailey/ultrasonicRanger', ultrasonic_callback)
+
+
+@mqtt.on_message()
+def once_message(client, userdata, msg):
+    print("on_message: " + msg.topic + " " + str(msg.payload, "utf-8"))
 
 @app.route('/')
 def index():
