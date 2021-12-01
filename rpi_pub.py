079ef79f8a3943dc695077b3340a6b146325cc16
Bailey <bcislows@usc.edu>
2021-11-30 18:19:28 -0800

    working except buzzer

--- rpi_pub.py
+++ rpi_pub.py
@@ -8,7 +8,19 @@ if __name__ == '__main__':
     client.connect(host="eclipse.usc.edu", port=1883, keepalive=60)
     client.loop_start()
     PORT = 3
+    count = 0
+    buzzer_on = False
     while True:
-        print(ultrasonicRead(PORT))
+        if buzzer_on:
+            # Ring buzzer
+            continue # Delete later
         client.publish("bailey/ultrasonicRanger", ultrasonicRead(PORT))
+        if ultrasonicRead(PORT) < 50:
+            count += 1
+            if count == 5:
+                buzzer_on = True
+                # Ring buzzer
+                # Shoot gun
+                count = 0
+                continue # Delete later
         time.sleep(1)
--- testServo.py
+++ testServo.py
@@ -5,8 +5,3 @@ servo = AngularServo(18, min_pulse_width=0.0006, max_pulse_width=0.0023)
 
 while (True):
     servo.angle = 90
-    sleep(2)
-    servo.angle = 0
-    sleep(2)
-    servo.angle = -90
-    sleep(2)
